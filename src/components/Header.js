45cf8931f2e7e52947b6c8478a9bfe1d947745c6
Iredia Omoike <72190008+Iredia35@users.noreply.github.com>
2024-09-18 13:23:03 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
      return (
           <div>
-               <h2>Debit Recodrer</h2>
+               <h2>MarvCakes Debit Recorder</h2>
           </div>
      )
 }
